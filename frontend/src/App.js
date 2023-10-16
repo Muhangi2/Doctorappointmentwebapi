f6e45e8662758f1d5015b03f9472b57dcb8439af
Muhangi2 <eliodamutiba@gmail.com>
2023-10-16 23:45:01 +0300

    routing

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import Layout from "./layout/Layout";
-
-const App = () => {
-  return <Layout />;
-};
-
-export default App;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Layout from "./layout/Layout";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Layout />
+    </>
+  );
+}
+
+export default App;
